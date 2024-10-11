7449bc4e58fe2ff5d9f253b7737bc4f1d72814a8
pear <66192078+a0908512313@users.noreply.github.com>
2024-10-11 11:47:12 +0800

    init commit

---
+++ e521. 106 彰雲嘉區複賽 - Q1 三角形.py
@@ -0,0 +1,20 @@
+n = int(input())
+results = []
+
+for _ in range(n):
+    sides = list(map(int, input().split()))
+    sides.sort()
+    a, b, c = sides
+
+    if a + b <= c:
+        results.append(0)
+    else:
+        if a == b:
+            results.append([1, 1])
+        else:
+            results.append([1, 0])
+for result in results:
+    if isinstance(result, list):
+        print(result[0], result[1])
+    else:
+        print(result)
