434c752fbbe6ee8f9fcb4b19062ad4b46bc03c4d
pear <66192078+a0908512313@users.noreply.github.com>
2024-10-11 10:44:24 +0800

    init commit

---
+++ a244. 新手訓練 ~ for + if .py
@@ -0,0 +1,12 @@
+n = int(input())
+data = list()
+for _ in range(n):
+    a, b, c = map(int, input().split())
+    if a == 1:
+        print(b+c)
+    elif a == 2:
+        print(b-c)
+    elif a == 3:
+        print(b*c)
+    elif a == 4:
+        print(b//c)
